a35e9ddb72234cec022be8719fece9bf042992d9
Miguel Ángel Ortega Hernández <miguelangel2005.o.h@gmail.com>
2024-03-21 00:03:21 -0500

    Front de la clase Inicio,Login y FrmRegistro

--- src/IGU/FrmRegistro.java
+++ src/IGU/FrmRegistro.java
@@ -7,70 +7,87 @@ import java.awt.event.ActionListener;
 
 import javax.swing.*;
 import Back.Huertas;
+import java.awt.*;
 
 public class FrmRegistro extends JFrame {
 
-    JLabel lblNombres, lblAncho, lblLargo;
+    JLabel lblNombres, lblAncho, lblLargo, titleLabel;
     JTextField txtNombres, txtAncho, txtLargo;
     JButton btnRegistrar, btnRegresar;
     JPanel panel;
 
     public FrmRegistro(Inicio frmInicio) {
+        
+        setTitle("Registrar Usuario");
+        setSize(500, 300);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+        getContentPane().setBackground(Color.WHITE);
 
-        // Panel
+        
         panel = new JPanel();
+        panel.setLayout(null);
+        panel.setBackground(Color.WHITE); 
         add(panel);
 
-        // inputs
-        lblNombres = new JLabel("Nombre");
+        
+        titleLabel = new JLabel("Registro de Huertas");
+        titleLabel.setBounds(180, 10, 200, 30);
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 20));
+        titleLabel.setForeground(new Color(0, 128, 0));
+        panel.add(titleLabel);
+
+    
+        lblNombres = new JLabel("Nombre:");
         lblNombres.setBounds(10, 50, 70, 25);
+        lblNombres.setForeground(new Color(0, 128, 0)); 
         panel.add(lblNombres);
-        txtNombres = new JTextField(15);
+        txtNombres = new JTextField();
+        txtNombres.setBounds(90, 50, 120, 25);
         panel.add(txtNombres);
 
-        lblAncho = new JLabel("Ancho");
-        lblAncho.setBounds(80, 80, 70, 25);
+        lblAncho = new JLabel("Ancho:");
+        lblAncho.setBounds(10, 90, 70, 25);
+        lblAncho.setForeground(new Color(0, 128, 0)); 
         panel.add(lblAncho);
-        txtAncho = new JTextField(15);
+        txtAncho = new JTextField();
+        txtAncho.setBounds(90, 90, 120, 25);
         panel.add(txtAncho);
 
-        lblLargo = new JLabel("Largo");
-        lblLargo.setBounds(80, 80, 70, 25);
+        lblLargo = new JLabel("Largo:");
+        lblLargo.setBounds(10, 130, 70, 25);
+        lblLargo.setForeground(new Color(0, 128, 0)); 
         panel.add(lblLargo);
-        txtLargo = new JTextField(15);
+        txtLargo = new JTextField();
+        txtLargo.setBounds(90, 130, 120, 25);
         panel.add(txtLargo);
 
-        // Botones
+        
         btnRegistrar = new JButton("Registrar");
-        btnRegistrar.setBounds(100, 500, 80, 25);
+        btnRegistrar.setBounds(90, 170, 100, 25);
+        btnRegistrar.setForeground(new Color(0, 128, 0)); 
         btnRegistrar.addActionListener(
-                e -> {
-                    Huertas huertas = new Huertas(txtNombres.getText(), Integer.parseInt(txtAncho.getText()), Integer.parseInt(txtLargo.getText()));
-                    JOptionPane.showMessageDialog(null, "Huerta registrada");
-                    ingresarHuerta(huertas);
-                    dispose();
-                    frmInicio.setVisible(true);
-                });
+            e -> {
+                Huertas huertas = new Huertas(txtNombres.getText(), Integer.parseInt(txtAncho.getText()), Integer.parseInt(txtLargo.getText()));
+                JOptionPane.showMessageDialog(null, "Huerta registrada");
+                ingresarHuerta(huertas);
+                dispose();
+                frmInicio.setVisible(true);
+            });
         panel.add(btnRegistrar);
 
         btnRegresar = new JButton("Regresar");
-        btnRegresar.setBounds(300, 500, 80, 25);
+        btnRegresar.setBounds(220, 170, 100, 25);
+        btnRegresar.setForeground(new Color(0, 128, 0)); 
         btnRegresar.addActionListener(
             new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e){
-                setVisible(false);
-                frmInicio.setVisible(true);
-
-            }
-        });
+                @Override
+                public void actionPerformed(ActionEvent e){
+                    setVisible(false);
+                    frmInicio.setVisible(true);
+                }
+            });
         panel.add(btnRegresar);
-
-        setTitle("Registrar Usuario");
-        setSize(500, 200);
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setVisible(true);
-        setResizable(false);
     }
 
     private static void ingresarHuerta(Huertas huerta) {
@@ -88,6 +105,5 @@ public class FrmRegistro extends JFrame {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 }
--- src/IGU/Login.java
+++ src/IGU/Login.java
@@ -2,71 +2,154 @@ package IGU;
 
 import javax.swing.*;
 
+import java.awt.Component;
+import java.awt.event.*;
+
 import Back.FileManager;
+import Back.VerifyUser;
 
 public class Login extends JFrame {
 
-    JTextField usuario;
-    JPasswordField contraseña;
-    JLabel user, pass, titulo;
-    JButton ingresar;
+    private JPanel jPanel1;
+    private JPanel jPanel3;
+    private JLabel jLabel1;
+    private JLabel jLabel2;
+    private JLabel jLabel3;
+    private JLabel jLabel4;
+    private JLabel jLabel5;
+    private JTextField jTextField1;
+    private JPasswordField jPasswordField1;
+    private JButton jButton1;
 
     public Login() {
-        // color mas oscuro de fondo
-        JPanel panel = new JPanel();
-        panel.setBackground(new java.awt.Color(225, 225, 225));
-        panel.setLayout(null);
-        add(panel);
-
-        titulo = new JLabel("Login");
-        titulo.setBounds(175, 15, 100, 30);
-        panel.add(titulo);
-
-        user = new JLabel("Usuario");
-        user.setBounds(100, 60, 100, 30);
-        panel.add(user);
-        usuario = new JTextField();
-        usuario.setBounds(200, 60, 100, 30);
-        panel.add(usuario);
-
-        pass = new JLabel("Contraseña");
-        pass.setBounds(90, 110, 100, 30);
-        panel.add(pass);
-        contraseña = new JPasswordField();
-        contraseña.setBounds(200, 110, 100, 30);
-        panel.add(contraseña);
-
-        ingresar = new JButton("Ingresar");
-        ingresar.setBounds(150, 160, 100, 30);
-        ingresar.addActionListener(
-                e -> {
-                    if (Back.VerifyUser.verify(usuario.getText(), new String(contraseña.getPassword()))) {
-                        dispose();
-                        String logIngreso = "Usuario: " + usuario.getText() + " Fecha: " + java.time.LocalDate.now()
-                                + " Hora: " + java.time.LocalTime.now() + "\n";
-                        FileManager.write("src/ingresos.log", logIngreso);
-                        new Inicio();
-                    } else {
-                        JOptionPane.showMessageDialog(null, "Usuario o contraseña incorrectos");
-                    }
-
-                });
-        panel.add(ingresar);
-
-        setTitle("Login");
-        setSize(400, 300);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
-        setResizable(false);
+        initComponents();
         setLocationRelativeTo(null);
-
     }
 
-    public static void initialize() {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
+    private void initComponents() {
+
+        jPanel1 = new JPanel();
+        jPanel3 = new JPanel();
+        jLabel1 = new JLabel();
+        jLabel3 = new JLabel();
+        jTextField1 = new JTextField();
+        jLabel4 = new JLabel();
+        jPasswordField1 = new JPasswordField();
+        jButton1 = new JButton();
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Login");
+
+        jPanel1.setLayout(new java.awt.BorderLayout());
+
+        jPanel3.setBackground(new java.awt.Color(255, 255, 255));
+
+        jLabel1.setFont(new java.awt.Font("Eras Demi ITC", 1, 30));
+        jLabel1.setForeground(new java.awt.Color(153, 255, 153));
+        jLabel1.setText("Agrocity");
+
+        jLabel3.setFont(new java.awt.Font("Dubai Medium", 0, 14));
+        jLabel3.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel3.setText("Usuario");
+
+        jLabel4.setFont(new java.awt.Font("Dubai Medium", 0, 14));
+        jLabel4.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel4.setText("Contraseña");
+
+        jButton1.setBackground(new java.awt.Color(153, 255, 153));
+        jButton1.setFont(new java.awt.Font("Dialog", 1, 18));
+        jButton1.setForeground(new java.awt.Color(255, 255, 255));
+        jButton1.setText("Iniciar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addGap(56, 56, 56)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(jPanel3Layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addGap(68, 68, 68))
+                    .addGroup(jPanel3Layout.createSequentialGroup()
+                        .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel4)
+                            .addComponent(jLabel3))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jTextField1)
+                            .addComponent(jPasswordField1))))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 106, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(63, 63, 63))
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addGap(27, 27, 27)
+                .addComponent(jLabel1)
+                .addGap(39, 39, 39)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(26, 26, 26)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(jPasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 25, Short.MAX_VALUE)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(93, 93, 93))
+        );
+
+        jPanel1.add(jPanel3, java.awt.BorderLayout.LINE_START);
+
+        jLabel2 = new JLabel();
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/FondoFinal.jpg")));
+        jPanel1.add(jLabel2, java.awt.BorderLayout.CENTER);
+
+        getContentPane().add(jPanel1, java.awt.BorderLayout.CENTER);
+
+        pack();
+    }                       
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+        String user = jTextField1.getText();
+        String pass = new String(jPasswordField1.getPassword());
+
+        if (Back.VerifyUser.verify(user, pass)) {
+            dispose();
+            String logIngreso = "Usuario: " + user + " Fecha: " + java.time.LocalDate.now()
+                    + " Hora: " + java.time.LocalTime.now() + "\n";
+            Back.FileManager.write("src/ingresos.log", logIngreso);
+            new Inicio().setVisible(true);
+        } else {
+            JOptionPane.showMessageDialog(null, "Usuario o contraseña incorrectos");
+        }
+    }           
+
+    public static void main(String args[]) {
+
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Login();
+                new Login().setVisible(true);
             }
         });
     }
